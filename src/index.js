42c567269a1d7c34ae2d946f75e468e642df1a0b
Brian Angulo <brianangulo96@gmail.com>
2021-05-20 19:47:26 -0400

    getting drid of react router(single comp app)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-
 import "assets/scss/material-kit-react.scss?v=1.10.0";
 
 // pages for this product
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 
-var hist = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={ProfilePage} />
-    </Switch>
-  </Router>,
+  <ProfilePage />,
   document.getElementById("root")
 );
